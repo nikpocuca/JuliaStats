7ca99772479664d08f76f701b932a7e4b9ab80d3
Nik Pocuca <nik@Niks-MacBook-Air.local>
2019-07-07 22:44:06 -0400

    finished animateEM file

--- src/animateEM.jl
+++ src/animateEM.jl
@@ -20,7 +20,7 @@ x_grid = -10:0.05:10;
 y_grid = copy(x_grid);
 
 contour(x_grid,y_grid, # place grids
-        (x,y)->Distributions.pdf(simple_binorm,[x,y]), # anynmous function to provide density for contour
+        (x,y)->Distributions.pdf(simple_binorm,[x,y]), # anon function to provide density calc for contour
         c = :plasma, # some colour
         fill = true # and fill
         )
@@ -32,22 +32,32 @@ contour(x_grid,y_grid, # place grids
 simple_binorm_2 = MvNormal(μ_2, Σ)
 
 contour(x_grid,y_grid, # place grids
-        (x,y)->Distributions.pdf(simple_binorm_2,[x,y]), # anynmous function to provide density for contour
+        (x,y)->Distributions.pdf(simple_binorm_2,[x,y]), # anon function to provide density for contour
         c = :plasma, # some colour
         fill = true # and fill
         )
 
 
+# ===========================================================================
+# Writing Julia Code for custom structures                                  #
+# ===========================================================================
 
-# Lets create a structure that incorporates a mixture of gaussians
-
+# Lets create a structure that represents a mixture of distributions
 
 mutable struct MixG
-        τ::Array{Float64,1}
-        dists
+        τ::Array{Float64,1} # tau is an array of mixing proportions
+        dists #is an array of distributions, I have left this untyped
 end
 
-function MixG(τ,dists)
+# Once you have defined the struct, it would be best
+# to write your own custom constructor.
+
+# For example, all of mixing proportions should add to 1,
+# if they do not when intialize you should get an error.
+
+function MixG(τ,dists) # note that this function name is the same as the constructor
+
+        # Check for convexity, the only constraint on a mix gaussian
         if(τ |> sum != 1.0)
                 error("not convex\n")
         end
@@ -57,7 +67,10 @@ end
 mix_gs = MixG([0.5,0.5],[simple_binorm,simple_binorm_2])
 
 
-# define a pdf function for it
+# Note that the package Distributions has a pdf function already.
+# first you need to import this function before you extend it.
+# import Distributions.pdf
+# now you can extend the pdf function to your mixture of Gaussians.
 function pdf(d_s::MixG,x,y)
         rez = 0.0;
         count = 1;
@@ -68,30 +81,46 @@ function pdf(d_s::MixG,x,y)
         return rez
 end
 
-
-# now plot and see that this function works
+# now plot and see that this new function works.
 contour(x_grid,y_grid, (x,y) -> pdf(mix_gs,x,y), c = :inferno)
 
+#indeed it does
+
+
+# ===========================================================================
+# Creating Animated Gifs using Julia                                        #
+# ===========================================================================
 
 # Lets practice animating this
 Σ = [1.0 0.0;
      0.0 1.0];
+
+# animate through the grid
+# note that this animation takes a while
 anim = @animate for i = -10:0.05:10
 
         μ = [i,i].*1.0;
         μ_2 = [i,-i]*1.0;
 
-
         simple_binorm_2 = MvNormal(μ_2, Σ)
         simple_binorm = MvNormal(μ, Σ)
+
         # Create mixture of gaussian structure
         mix_gs = MixG([0.5,0.5],[simple_binorm,simple_binorm_2])
 
         # now plot and see that this function works
-        contour(x_grid,y_grid, (x,y) -> pdf(mix_gs,x,y), c = :inferno,
-        background_color_outside= :black,background_color_inside= :black)
+        contour(x_grid,y_grid,
+                (x,y) -> pdf(mix_gs,x,y),
+                c = :inferno,
+                background_color_outside= :black,
+                background_color_inside= :black)
 
-end every 1
+end every 1 # every 1 or 2 or ... k steps you can save picture instead of all of them
+# note that this animation takes a while
 
-# save the animation
-gif(anim, "/Users/nik/anim_fps15.gif", fps = 45)
+# save the animation put it anywhere you would like and name it accordingly
+name = "anim_fps15.gif"
+loc = "/Users/nik/$name"
+
+# save gif of animation.
+gif(anim, loc, fps = 45)
